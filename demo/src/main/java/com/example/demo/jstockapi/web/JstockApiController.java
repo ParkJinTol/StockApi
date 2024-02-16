2c13d34d7d5f76963d9dc882173e6a561b887141
ParkJinSeok <pjsjja458@naver.com>
2024-02-17 01:59:13 +0900

    test

--- demo/src/main/java/com/example/demo/jstockapi/web/JstockApiController.java
+++ demo/src/main/java/com/example/demo/jstockapi/web/JstockApiController.java
@@ -1,12 +1,17 @@
 package com.example.demo.jstockapi.web;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JstockApiController {
   @GetMapping("/")
   public String Jstock() {
+    return "hello";
+  }
+
+  @GetMapping("/test")
+  public String Jstock1() {
     return "hello";
   }
 
